4a6cb30497330055194afe97727c468e7b6c3d3d
pragdua <72184191+pragdua@users.noreply.github.com>
2021-09-06 22:01:23 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,31 @@
+const dateOfBirth = document.querySelector("#date-of-birth");
+const luckyNumber = document.querySelector("#lucky-number");
+const checkNumberButton = document.querySelector("#check-number");
+const outputBox = document.querySelector("#output-box");
+
+const checkDateIsLucky = () => {
+  const dob = dateOfBirth.value;
+  const sum = calcSum(dob);
+
+  if (sum && dob) {
+    if (sum % luckyNumber.value === 0) {
+      outputBox.innerText = "your birthday is lucky! 🎉🥳";
+    } else {
+      outputBox.innerText = "not lucky, sorry 😢";
+    }
+  } else {
+    outputBox.innerText = "please enter both fields 😠😒";
+  }
+};
+
+const calcSum = (dob) => {
+  const newDate = dob.replaceAll("-", "");
+  let sum = 0;
+  for (let i = 0; i < newDate.length; i++) {
+    sum += Number(newDate.charAt(i));
+  }
+
+  return sum;
+};
+
+checkNumberButton.addEventListener("click", checkDateIsLucky);
